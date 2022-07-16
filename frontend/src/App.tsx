a8fcfa1a1bad990986b8776ed041222cfd833b9c
Moises Nantes <moisesnantes@gmail.com>
2022-07-15 21:22:26 -0400

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import NotificationBotton from "./components/NotificationBotton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 function App() {
-  return(
+  return (
     <>
-      <h1>Olá mundo!</h1>
-     <NotificationBotton/>
-     <NotificationBotton/>
-      
-      
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+
+
     </>
   )
 }
